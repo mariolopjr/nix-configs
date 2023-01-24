97b6a86707761f510cd5f4424bab8f37de5adf4e
Mario Lopez <mario@techmunchies.net>
2023-01-23 20:56:57 -0500

    fix font name

--- home/snow/features/desktop/common/fonts.nix
+++ home/snow/features/desktop/common/fonts.nix
@@ -3,7 +3,7 @@
     enable = true;
     monospace = {
       family = "Meslo Nerd Font";
-      package = pkgs.nerdfonts.override { fonts = [ "Meslo Nerd Font" ]; };
+      package = pkgs.nerdfonts.override { fonts = [ "Meslo" ]; };
     };
     regular = {
       family = "Fira Sans";
