51f22b2e654c75071aed2fce5585876216f674cf
Mario Lopez <mario@techmunchies.net>
2023-01-24 20:59:17 -0500

    fix encrypted root

--- hosts/common/optional/encrypted-root.nix
+++ hosts/common/optional/encrypted-root.nix
@@ -2,6 +2,6 @@
 let hostname = config.networking.hostName;
 in {
   boot.initrd ={
-    luks.devices."${hostname}".device = "/dev/disk/by-partlabel/${hostname}_crypt";
+    luks.devices."${hostname}_crypt".device = "/dev/disk/by-partlabel/${hostname}";
   };
 }
